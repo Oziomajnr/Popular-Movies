c628ed99ade508de4f973e9d4098bc34b97106fa
OGBE OZIOMA JNR <ogbeoziomajnr@yahoo.com>
2017-05-13 08:05:16 +0100

    add trailer and trailer response class

--- app/src/main/java/com/example/ogbeoziomajnr/popularmovies/Model/MovieTrailerResponse.java
+++ app/src/main/java/com/example/ogbeoziomajnr/popularmovies/Model/MovieTrailerResponse.java
@@ -4,5 +4,29 @@ package com.example.ogbeoziomajnr.popularmovies.Model;
  * Created by SQ-OGBE PC on 12/05/2017.
  */
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 public class MovieTrailerResponse {
+    @SerializedName("id")
+    private String id;
+    @SerializedName("results")
+    private List<MovieTrailers> results;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public List<MovieTrailers> getResults() {
+        return results;
+    }
+
+    public void setResults(List<MovieTrailers> results) {
+        this.results = results;
+    }
 }
--- app/src/main/java/com/example/ogbeoziomajnr/popularmovies/Model/MovieTrailers.java
+++ app/src/main/java/com/example/ogbeoziomajnr/popularmovies/Model/MovieTrailers.java
@@ -3,6 +3,72 @@ package com.example.ogbeoziomajnr.popularmovies.Model;
 /**
  * Created by SQ-OGBE PC on 12/05/2017.
  */
+import com.google.gson.annotations.SerializedName;
 
 public class MovieTrailers {
+    @SerializedName("id")
+    private String id;
+
+    @SerializedName("key")
+    private String key;
+
+    @SerializedName("name")
+    private String name;
+
+    @SerializedName("site")
+    private String site;
+
+    @SerializedName("size")
+    private String size;
+
+    @SerializedName("type")
+    private String type;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
