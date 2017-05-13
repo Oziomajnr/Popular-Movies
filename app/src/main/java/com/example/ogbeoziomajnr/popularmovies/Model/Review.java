4cb67f7beeeae70b2a63cde42c90062ff843f0b2
OGBE OZIOMA JNR <ogbeoziomajnr@yahoo.com>
2017-05-13 08:04:56 +0100

    add review and review response class

--- app/src/main/java/com/example/ogbeoziomajnr/popularmovies/Model/Review.java
+++ app/src/main/java/com/example/ogbeoziomajnr/popularmovies/Model/Review.java
@@ -1,8 +1,23 @@
 package com.example.ogbeoziomajnr.popularmovies.Model;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 /**
  * Created by SQ-OGBE PC on 12/05/2017.
  */
 
 public class Review {
+
+    @SerializedName("url")
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
--- app/src/main/java/com/example/ogbeoziomajnr/popularmovies/Model/ReviewResponse.java
+++ app/src/main/java/com/example/ogbeoziomajnr/popularmovies/Model/ReviewResponse.java
@@ -1,8 +1,24 @@
 package com.example.ogbeoziomajnr.popularmovies.Model;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 /**
  * Created by SQ-OGBE PC on 12/05/2017.
  */
 
 public class ReviewResponse {
+
+    @SerializedName("results")
+    private List<Review> results;
+
+
+    public List<Review> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Review> results) {
+        this.results = results;
+    }
 }
