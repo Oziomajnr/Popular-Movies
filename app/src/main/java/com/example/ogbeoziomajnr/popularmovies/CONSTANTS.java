6c41dfa39f1c57d1009147cc63d9611c30d74a11
OGBE OZIOMA JNR <ogbeoziomajnr@yahoo.com>
2017-05-16 13:40:38 +0100

    Change name of Constant file

--- app/src/main/java/com/example/ogbeoziomajnr/popularmovies/CONSTANTS.java
+++ app/src/main/java/com/example/ogbeoziomajnr/popularmovies/CONSTANTS.java
@@ -4,7 +4,7 @@ package com.example.ogbeoziomajnr.popularmovies;
  * Created by SQ-OGBE PC on 15/04/2017.
  */
 
-public class CONSTANTS {
+public class Constants {
     public static final String IMAGE_BASE_URL = "http://image.tmdb.org/t/p/w342";
     public static final String YOUTUBE_BASE_URL = "https://www.youtube.com/watch?v=";
     public   enum category{
